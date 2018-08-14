37683f11da59a56bf3b6a5e1423f4c1f7169ce38
eric-yoo <ajoije729@gmail.com>
2018-08-14 22:44:57 +0900

    df to tensor

--- model.py
+++ model.py
@@ -57,7 +57,8 @@ def event_adapter(batch_size):
     train_data_file = os.path.join(filepath, "train_data.csv")
     n = sum(1 for line in open(train_data_file)) - 1 #number of records in file (excludes header)
     skip = sorted(random.sample(range(1,n+1),n-batch_size)) #the 0-indexed header will not be included in the skip list
-    event_records = pandas.read_csv(train_data_file, skiprows=skip, engine='python')
+    df = pandas.read_csv(train_data_file, skiprows=skip, engine='python')
+    event_records = df.values
 
     query_answers = []
     query_answers.append(np.random.randint(0, answer_dim[0], batch_size))
